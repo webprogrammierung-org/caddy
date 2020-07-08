83a9dbd71e8de20007d1fcad862156178e0bb50d
Thorsten Klöhn <thorstenkloehn@gmail.com>
2020-07-08 21:14:20 +0200

    Neue Server

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ rm /hallo/gitea.zip
 rm /hallo/dump.xml
 cd /gitea
 ./gitea dump -f /hallo/gitea.zip
-cd /var/www/html/maintenance
+cd /mediawiki/maintenance
 php dumpBackup.php --full > /hallo/dump.xml
